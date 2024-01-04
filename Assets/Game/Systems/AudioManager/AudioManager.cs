3357c28c7489ac2154db2f18d7f5e3714048158c
Delta <saf207207@gmail.com>
2024-01-05 01:33:56 +0600

    Added audio manager and settings layout

---
+++ Assets/Game/Systems/AudioManager/AudioManager.cs
@@ -0,0 +1,79 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+    public static AudioManager inst { get; private set; }
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource soundsSource;
+    [SerializeField] private AudioClip musicClip;
+    [Header("Clamping")]
+    [SerializeField] private int maxQueriesPerType;
+    [SerializeField] private AudioClip avoidClip;
+    private Dictionary<AudioClip, int> queriesPlaying = new Dictionary<AudioClip, int>();
+    private Stack<PlayingQueryTime> queriesStack = new Stack<PlayingQueryTime>();
+    private float musicVolume;
+    private float soundVolume;
+    private void Awake() {
+        if (inst != null && inst != this) {
+            Destroy(this);
+        } else {
+            inst = this;
+            DontDestroyOnLoad(gameObject);
+            PlayMusic(musicClip);
+        }
+    }
+
+    public void SetMusicVolume(float value) {
+        musicVolume = value;
+        musicSource.volume = musicVolume;
+    }
+
+    public void SetSoundVolume(float value) {
+        soundVolume = value;
+        soundsSource.volume = soundVolume;
+    }
+
+    public void Play(AudioQuery query) {
+        if (query.clip == null) return;
+        if (queriesPlaying.ContainsKey(query.clip)) {
+            if (query.clip != avoidClip && queriesPlaying[query.clip] >= maxQueriesPerType) return;
+        }
+        soundsSource.pitch = Random.Range(1 - query.pitchVariance, 1 + query.pitchVariance);
+        soundsSource.PlayOneShot(query.clip);
+        float timeFinished = Time.time + query.clip.length;
+        PlayingQueryTime stamp = new PlayingQueryTime() { clip = query.clip , timestamp = timeFinished };
+        if (queriesPlaying.ContainsKey(query.clip)) {
+            queriesPlaying[query.clip]++;
+            queriesStack.Push(stamp);
+        } else {
+            queriesPlaying.Add(query.clip, 1);
+            queriesStack.Push(stamp);
+        }
+    }
+
+    public void PlayMusic(AudioClip clip) {
+        if (musicSource.isPlaying) return;
+        musicSource.clip = clip;
+        musicSource.Play();
+    }
+    public void StopMusic() {
+        musicSource.Stop();
+    }
+
+    private void CheckStack() {
+        if (queriesStack.Count == 0) return;
+        if (queriesStack.Peek().timestamp <= Time.time) {
+            queriesPlaying[queriesStack.Peek().clip]--;
+            queriesStack.Pop();
+        }
+    }
+
+    private void Update() {
+        CheckStack();
+    }
+
+    private class PlayingQueryTime {
+        public AudioClip clip;
+        public float timestamp;
+    }
+}
--- Assets/Menu/MenuManager.cs
+++ Assets/Menu/MenuManager.cs
@@ -2,12 +2,23 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour {
     [SerializeField] private string targetScene;
+    [SerializeField] private GameObject settingsPanel;
+    
+
     public void Play() {
         SceneLoader.inst.LoadScene(targetScene);
     }
 
     public void Settings() {
-        //Open settings menu
+        settingsPanel.SetActive(!settingsPanel.activeSelf);
+    }
+
+    public void MusicVolumeSet(float value) {
+        
+    }
+
+    public void SoundVolumeSet(float value) {
+
     }
 
     public void Exit() {
