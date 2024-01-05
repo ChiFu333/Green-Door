9b1f0260d42ac4e03ac288df2f54e4283d00c4a2
Delta <saf207207@gmail.com>
2024-01-05 23:01:59 +0600

    Yet another fix of combination heuristic Made cursor's positioning a little better

--- Assets/Game/Player/Inventory/ItemCombinationSO.cs
+++ Assets/Game/Player/Inventory/ItemCombinationSO.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ItemCombination", menuName = "Items/ItemCombination")]
 public class ItemCombinationSO : ScriptableObject {
     [field: SerializeField] public ItemDataSO firstObject { get; private set; }
-    [field:SerializeField] public ItemDataSO secondObject { get; private set; }
-    [field:SerializeField] public ItemDataSO resultingObject { get; private set; }
+    [field: SerializeField] public ItemDataSO secondObject { get; private set; }
+    [field: SerializeField] public ItemDataSO resultingObject { get; private set; }
 
     public bool IsItemRequired(ItemDataSO data) {
         return firstObject == data || secondObject == data;
@@ -14,8 +14,12 @@ public class ItemCombinationSO : ScriptableObject {
     public bool IsEnoughItems(List<ItemDataSO> items) {
         bool firstFound = false, secondFound = false;
         for (int i = 0; i < items.Count; i++) {
-            if (items[i] == firstObject) { firstFound = true; continue; }
-            if (items[i] == secondObject) { secondFound = true; continue; }
+            if (!firstFound) {
+                if (items[i] == firstObject) { firstFound = true; continue; }
+            }
+            if (!secondFound) {
+                if (items[i] == secondObject) { secondFound = true; continue; }
+            }
         }
         return firstFound && secondFound;
     }
