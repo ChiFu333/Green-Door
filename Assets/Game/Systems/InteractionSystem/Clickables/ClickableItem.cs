9c76ac138e33a739b98ed75f1d33a81ad7b6d91b
ChiFu <Email2Vova@yandex.ru>
2024-01-07 03:34:08 +0300

    Много МНого чего

--- Assets/Game/Systems/InteractionSystem/Clickables/ClickableItem.cs
+++ Assets/Game/Systems/InteractionSystem/Clickables/ClickableItem.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class ClickableItem : ClickableThing {
     [SerializeField] private ItemDataSO itemData;
     [SerializeField] private AudioClip itemTaked;
-
+    [SerializeField] private UnityEvent AfterUseCallback = new UnityEvent();
     public override void HandleClick() {
         base.HandleClick();
         Player.inst.controller.MoveTo(transform.position, () => {
             PlayerInventory.inst.PickupItem(new Item(itemData));
             AudioManager.inst.Play(new AudioQuery(itemTaked));
             FindObjectOfType<MagicOrchestrator>().items[key] = true;
+            AfterUseCallback.Invoke();
             Destroy(gameObject);
         });
     }
